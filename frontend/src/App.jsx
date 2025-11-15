6e563176ac5824306d98c09f674ca1ec46d44f42
Your Name <you@example.com>
2025-11-15 22:00:15 +0200

    setup routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,26 @@
 import { Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+
 import Home from "./pages/Home";
 import Facilities from "./pages/Facilities";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/facilities" element={<Facilities />} />
-      {/* catch-all route */}
+      {/* Routes that use the main layout + navbar */}
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/facilities" element={<Facilities />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+
+      {/* Standalone route (without layout if you want a clean login page) */}
+      <Route path="/login" element={<Login />} />
+
+      {/* 404 */}
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
